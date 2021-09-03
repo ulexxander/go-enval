af034dd1ab19971c2e5225ffeae6e343f294e389
Alexander Ustyugov <ulexxander@gmail.com>
2021-09-03 19:19:12 +0200

    TestDefaults - testing *WithDefault methods behavior

--- enval_test.go
+++ enval_test.go
@@ -116,3 +116,95 @@ func TestValuesAndErrors(t *testing.T) {
 		})
 	}
 }
+
+func TestDefaults(t *testing.T) {
+	s := enval.NewLookuper()
+	vars := map[string]string{
+		"STRING_PRESENT": ":80",
+		// "STRING_MISSING": "actually required",
+
+		"INT_PRESENT": "16",
+		"INT_INVALID": "b4dint34",
+		// "INT_MISSING":     "actually required",
+
+		"BOOL_PRESENT": "true",
+		"BOOL_INVALID": "nOTtRueOrFalsE",
+		// "BOOL_MISSING": "actually required",
+
+		"CUSTOM_PRESENT": `{"abc": 456}`,
+		"CUSTOM_INVALID": `}"abc": 456{`,
+		// "CUSTOM_MISSING": "actually required",
+	}
+	s.LookupFunc = func(key string) (string, bool) {
+		val, present := vars[key]
+		return val, present
+	}
+
+	type valStringDef struct{ expected, def string }
+	type valIntDef struct{ expected, def int }
+	type valBoolDef struct{ expected, def bool }
+	type valCustomDef struct{ expected, def abc }
+
+	tt := []struct {
+		key       string
+		valType   valType
+		valString valStringDef
+		valInt    valIntDef
+		valBool   valBoolDef
+		valCustom valCustomDef
+		hasErr    bool
+	}{
+		{key: "STRING_PRESENT", valType: valString, valString: valStringDef{":80", ":10030"}},
+		{key: "STRING_MISSING", valType: valString, valString: valStringDef{":10030", ":10030"}},
+		{key: "INT_PRESENT", valType: valInt, valInt: valIntDef{16, 550}},
+		{key: "INT_INVALID", valType: valInt, hasErr: true},
+		{key: "INT_MISSING", valType: valInt, valInt: valIntDef{550, 550}},
+		{key: "BOOL_PRESENT", valType: valBool, valBool: valBoolDef{true, false}},
+		{key: "BOOL_INVALID", valType: valInt, hasErr: true},
+		{key: "BOOL_MISSING", valType: valInt, valBool: valBoolDef{true, true}},
+		// {key: "CUSTOM_PRESENT", valType: valCustom, valCustom: valCustomDef{abc{Abc: 456}, abc{Abc: 999}},
+		{key: "CUSTOM_INVALID", valType: valCustom, hasErr: true},
+		{key: "CUSTOM_MISSING", valType: valCustom, valCustom: valCustomDef{abc{Abc: 999}, abc{Abc: 999}}},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.key, func(t *testing.T) {
+			if tc.valType == valString {
+				s := s.StringWithDefault(tc.key, tc.valString.def)
+				if s != tc.valString.expected {
+					t.Fatalf("expected string value to be %s, got: %s", tc.valString.expected, s)
+				}
+			}
+
+			if tc.valType == valInt {
+				i := s.IntWithDefault(tc.key, tc.valInt.def)
+				if i != tc.valInt.expected {
+					t.Fatalf("expected int value to be %d, got: %d", tc.valInt.expected, i)
+				}
+			}
+
+			if tc.valType == valBool {
+				b := s.BoolWithDefault(tc.key, tc.valBool.def)
+				if b != tc.valBool.expected {
+					t.Fatalf("expected bool value to be %t, got: %t", tc.valBool.expected, b)
+				}
+			}
+
+			if tc.valType == valCustom {
+				v, ok := s.CustomWithDefault(tc.key, tc.valCustom.def, abcParseFunc).(abc)
+				if ok && v.Abc != tc.valCustom.expected.Abc {
+					t.Fatalf("expected custom abc value to be %d, got: %d", tc.valCustom.expected.Abc, v.Abc)
+				}
+			}
+
+			err := s.ErrByVariable[tc.key]
+			if !tc.hasErr && err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if tc.hasErr && err == nil {
+				t.Fatalf("expected error here")
+			}
+		})
+	}
+}
